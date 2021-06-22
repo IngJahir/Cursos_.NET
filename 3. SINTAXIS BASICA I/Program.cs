ecd87da1060357197a673569e434c3b0cf033b42
IngJahir <jahirtautiva@gmail.com>
2021-06-21 22:29:43 -0500

    Sintaxis básica I

---
+++ 3. SINTAXIS BASICA I/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+--------------------------
+VIDEO 3: SINTAXIS BASICA I
+--------------------------
+-> Tipos de comentarios en C#:
+    * Comentarios: Inicializan con //
+    * Bloque de codigo
+
+-> Identificadores: 
+    * Nombres que se utilizan para identificar los elementos de un programa:
+      Namespace, Clases, Metodos, Variables, constantes. 
+   * Solo se pueden usar letras mayusculas y minusculas, numeros y guiones bajos.   
+   * Deben comenzar por una letra o un guion bajo. JAMAS por un numero
+   * No se deben utilizar palabras claves o palabras reservadas: using, namespace, class..
+*/
+
+using System;
+
+namespace _3._SINTAXIS_BASICA_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Esto imprime en la consola
+            Console.WriteLine("Bienvenidos al curso de C#");
+        }
+    }
+}
