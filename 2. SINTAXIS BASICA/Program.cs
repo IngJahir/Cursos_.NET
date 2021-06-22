cd5723ce6c2f5c0417942d818caa0ac8e7a96c29
IngJahir <jahirtautiva@gmail.com>
2021-06-21 21:57:17 -0500

    Sintaxis básica

---
+++ 2. SINTAXIS BASICA/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+VIDEO 2: SINTAXIS BASICA
+-------------------------
+Tipos de aplicaciones en C#:
+
+1. De consola : Aplicaxciones que se ejecutan en una ventana de comandos.
+2. De escritorio: Aplicaciones que se ejecutan en una IGU (Interfaz Grafica de Usuario).
+3. Web:Aplicaciones que se ejecutan en un navegador.
+
+-> Case sensitive: Distingue las mayusculas de las minusculas.
+-> Los Bloques de codigo estan delimitados por llaves {}
+-> En un NameSpace NO PUEDEN haber dos clases con el mismo nombre.
+-> Clases propias: Clases que se crean cuando creamos una aplicacion
+-> Clases predefinidas: Clases que ya vienen con el lenguaje de programacion o Bibliotecas de clases o API
+
+**/
+using System;
+
+namespace _2._SINTAXIS_BASICA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos al curso de C#");
+        }
+    }
+}
