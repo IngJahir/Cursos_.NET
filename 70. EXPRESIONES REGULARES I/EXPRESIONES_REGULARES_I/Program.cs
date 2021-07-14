6fff4976a97abf31aa64baa381113d1b2fc74fe2
IngJahir <jahirtautiva@gmail.com>
2021-07-13 20:45:06 -0500

    Expresiones regulares en C#

---
+++ 70. EXPRESIONES REGULARES I/EXPRESIONES_REGULARES_I/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+----------------------------------
+VIDEO 70: EXPRESIONES REGULARES I
+----------------------------------
+-> Expresion regular: Secuencia de caracteres que funcionan como un patron de busqueda.
+                      buscan cadenas de caracteres como letras, numeros u otros caracteres.
+
+-> Se utilizan para validacion de formularios, busqueda en ficheros externos, busqueda en BBDD,
+   comprobacion de entrada de usuarios.
+
+-> Clase utiles: Regex, Match, MatchCollection, GroupCollection
+-> Metodos utiles: Matches
+-> Propiedades utiles: Groups
+*/
+using System;
+using System.Text.RegularExpressions;
+
+namespace EXPRESIONES_REGULARES_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Mi nombre es Juan y mi numero de telefono es (+34)123-45-67 y mi codigo postal es 29679";
+            string patron_1 = "[J]";
+            string patron_2 = @"\d"; // Corresponde a los numeros segun la documentacion
+            string patron_3 = @"\d{3}-\d{2}-\d{2}"; // Formato de numero de telefono 3-2-2;
+
+
+            // Creacion de espresion regular
+            // -----------------------------
+            Regex oRegex_1 = new Regex(patron_1);
+            Regex oRegex_2 = new Regex(patron_2);
+            Regex oRegex_3 = new Regex(patron_3);
+
+            // Buscar y almacenar las coincidencias
+            // ------------------------------------
+            MatchCollection oMatch_1 = oRegex_1.Matches(frase);
+            MatchCollection oMatch_2 = oRegex_2.Matches(frase);
+            MatchCollection oMatch_3 = oRegex_3.Matches(frase);
+
+            // Mensaja de encontrado
+            // ---------------------
+            if (oMatch_1.Count > 0) Console.WriteLine("Se ha encontrado una J");
+            else Console.WriteLine("No se ha encontrado una J");
+
+            if (oMatch_2.Count > 0) Console.WriteLine("Se ha encontrado numeros");
+            else Console.WriteLine("No se ha encontrado numeros");
+
+            if (oMatch_3.Count > 0) Console.WriteLine("Se ha encontrado un numero de telefono");
+            else Console.WriteLine("No se ha encontrado un numero de telefono");
+        }
+    }
+}
---
+++ 71. EXPRESIONES REGULARES II/EXPRESIONES_REGULARES_II/EXPRESIONES_REGULARES_II/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+-----------------------------------
+VIDEO 71: EXPRESIONES REGULARES II
+-----------------------------------
+-> Usando operador | y cuantificadores
+-> Ver generadores de expresiones regulares por internet!!
+
+*/
+using System;
+using System.Text.RegularExpressions;
+
+namespace EXPRESIONES_REGULARES_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo 1: Usando el operador o
+            // -------------------------------
+            string frase = "Mi nombre es Juan y mi numero de telefono es (+34)123-45-67, (+44)321-54-76  y mi codigo postal es 29679";
+            string patron_1 = @"\+34|\+44";
+
+            Regex oRegex_1 = new Regex(patron_1);
+            MatchCollection oMatch_1 = oRegex_1.Matches(frase);
+
+            if (oMatch_1.Count > 0) Console.WriteLine("Se ha encontrado numero de telefono de UK o de España");
+            else Console.WriteLine("No se ha encontrado numero de telefono ni de España ni de UK");
+
+            // Ejemplo 2: Usando una pagina web
+            // ---------------------------------
+            string web = "Mi web es https://wwww.pildorasinformaticas.es";
+            string patron_2 = "https?://(www.)? pildorasinformaticas.es"; // ? Esta una o ninguna
+            
+            Regex oRegex_2 = new Regex(patron_2);
+            MatchCollection oMatch_2 = oRegex_2.Matches(web);
+
+            if (oMatch_2.Count > 0) Console.WriteLine("Se ha encontrado una web");
+            else Console.WriteLine("No se ha encontrado ninguna web");
+
+            // Ejemplo 3: Usando un correo electronico
+            // ----------------------------------------
+            string email = "cursos@pildorasinformaticas.es";
+            string re1 = ".*?";
+            string re2 = "(@)";
+            string re3 = ".*?";
+            string re4 = "(\\.)";
+
+            Regex r = new Regex(re1 + re2 + re3 + re4,RegexOptions.IgnoreCase |RegexOptions.Singleline);
+            Match m = r.Match(email);
+            if (m.Success) Console.WriteLine("Email correcto");
+            else Console.WriteLine("Email NO CORECCTO");
+
+        }
+    }
+}
